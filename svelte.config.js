fbaa31579b0d5e804c5d994e2e13cfb0f8719850
jill64 <intents.turrets0h@icloud.com>
2024-05-13 11:37:10 +0900

    add deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@jill64/sveltekit-adapter-aws';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
